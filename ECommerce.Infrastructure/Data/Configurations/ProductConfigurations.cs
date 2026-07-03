2c3c7680fa3a958558204b724a071f3e9bef4476
Ola Haggag <143203892+Ola-Haggag@users.noreply.github.com>
2026-07-03 04:23:14 +0300

    Add project files.

---
+++ ECommerce.Infrastructure/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Data.Configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.PictureUrl).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(500);
+            
+            
+            builder.Property(x => x.Price).HasColumnType("decimal(10,2)");
+        }
+    }
+}
---
+++ ECommerce.Infrastructure/Data/StoreDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Data
+{
+    public class StoreDbContext:DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductsBrands { get; set; }
+        public DbSet<ProductsType> ProductsTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+        }
+    }
+}
